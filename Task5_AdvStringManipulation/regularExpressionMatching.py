2e00c10f2ebb694a29dc8272f482358da59edefe
Malcom Otieno Adrian <154547110+Swiish21@users.noreply.github.com>
2024-10-29 16:37:16 +0300

    Update regularExpressionMatching.py

--- Task5_AdvStringManipulation/regularExpressionMatching.py
+++ Task5_AdvStringManipulation/regularExpressionMatching.py
@@ -1,22 +1,16 @@
-def is_match(s: str, p: str) -> bool: # we define a function called is_match that returns a boolean value, we assign it 2 parametres s(string) and p(string)
-    #s is for the string to be checked and p is for the regular expression to compare it to.
+def is_match(text: str, pattern: str) -> bool:
+    
+    if not pattern:
+        return not text
 
-    if len(p) == 0: # here we deal with the case where the regular expression  and the string are empty,
-        return len(s) == 0 #if they are both empty we return true, otherwise false
+    first_match = bool(text) and (pattern[0] == text[0] or pattern[0] == '.')
 
-
-    if len(p) == 1: # here we deal with the case where the regular expression has only one character
-        return len(s) == 1 and (p[0] == s[0] or (p[0] == '.' and s[0] != '\0')) #if the first character of the regular expression is the same as the first character of the string, we return true, otherwise false
-
-    # here we deal with the case where the regular expression has more than one character
-    if p[1] == '*': #if the second character of the regular expression is a star, we check if the first character of the string matches the regular expression
-        return (p[0] == s[0] or (p[0] == '.' and s[0] != '\0')) and is_match(s, p[2:]) or (s != '' and (p[0] == s[0] or (p[0] == '.' and s[0] != '\0')) and is_match(s[1:], p))
-
-
-    return (s != '' and (p[0] == s[0] or (p[0] == '.' and s[0] != '\0')) and is_match(s[1:], p[1:])) #if the first character of the regular expression is the same as the first character of the string, we return true, otherwise false
-
-#test cases for our function
-print(is_match("aa", "a"))  # Output: False , reason is the expression wants a single character, the string has two characters
-print(is_match("aa", "a*"))  # Output: True , reason the expression has an asterisk, so it doesn't matter how many 'a's are in the string, it will match the condition
-print(is_match("ab", ".*"))  # Output: True , reason is the expression has a dot and then an asterisk, meaning any character(s) are allowed,
-print(is_match("mississippi", "mis*is*p*."))  # Output: False , reason is the input string has too many 'i' and 's', but the expression doesn't allow it to have all these 'i's and 's's, hence it will return false.
+    if len(pattern) >= 2 and pattern[1] == '*':
+        return is_match(text, pattern[2:]) or first_match and is_match(text[1:], pattern)
+    else:
+        return first_match and is_match(text[1:], pattern[1:])
+        
+print(is_match("aa", "a"))  # False
+print(is_match("aa", "a*"))  # True
+print(is_match("ab", ".*"))  # True
+print(is_match("mississippi", "mis*is*p*."))  # False
