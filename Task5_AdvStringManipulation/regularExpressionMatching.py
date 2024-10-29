5d6fb0d3ad34e1573be868bf4c42e45256f020bf
Malcom Otieno Adrian <154547110+Swiish21@users.noreply.github.com>
2024-10-29 17:01:46 +0300

    Update regularExpressionMatching.py

--- Task5_AdvStringManipulation/regularExpressionMatching.py
+++ Task5_AdvStringManipulation/regularExpressionMatching.py
@@ -3,13 +3,14 @@ def is_match(text: str, pattern: str) -> bool: #we defined a function is_match t
     #below here we deal with the case where if the pattern is empty, if it is , it means that our text must also be empty for it match, else it returns a false value. 
     if not pattern: 
         return not text
-
+    #now we deal with the case where the first character of the text matches the first character of the pattern, also added a case where it will match if the first character is a (.) which stands for any character.
     first_match = bool(text) and (pattern[0] == text[0] or pattern[0] == '.')
-
-    if len(pattern) >= 2 and pattern[1] == '*':
-        return is_match(text, pattern[2:]) or first_match and is_match(text[1:], pattern)
+    #here we use an if statement to check two conditions, if the length of the pattern is graeter than or equal to 2 and the second symbol in the pattern is (*)
+    if len(pattern) >= 2 and pattern[1] == '*': #we'll have two scenarios below.
+        return is_match(text, pattern[2:]) or first_match and is_match(text[1:], pattern) 
+        #is_match(text, pattern[2:0]) covers the case where the wildcard matches zero occurences of the preceeding character and first_match and is_match(text[1:], pattern) covers the case where the wildcard matches one or more occurence of the preceeding charcter.
     else:
-        return first_match and is_match(text[1:], pattern[1:])
+        return first_match and is_match(text[1:], pattern[1:]) #if the second character of the pattern is not a (*), its a normal character match and the 'first_match' condition ensures the first character match then it checks whether the remaining characters match with the pattern.
         
 print(is_match("aa", "a"))  # False
 print(is_match("aa", "a*"))  # True
