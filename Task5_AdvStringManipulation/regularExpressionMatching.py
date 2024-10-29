c3f1f669f11abb3b7aeb6111ac2509406c55e6f6
Malcom Otieno Adrian <154547110+Swiish21@users.noreply.github.com>
2024-10-29 16:45:20 +0300

    Update regularExpressionMatching.py

--- Task5_AdvStringManipulation/regularExpressionMatching.py
+++ Task5_AdvStringManipulation/regularExpressionMatching.py
@@ -1,6 +1,7 @@
-def is_match(text: str, pattern: str) -> bool:
-    
-    if not pattern:
+def is_match(text: str, pattern: str) -> bool: #we defined a function is_match that has two parameters, one for the string to be checked and one for the regex pattern, the function is supposed to return either true or false(boolean)
+
+    #below here we deal with the case where if the pattern is empty, if it is , it means that our text must also be empty for it match, else it returns a false value. 
+    if not pattern: 
         return not text
 
     first_match = bool(text) and (pattern[0] == text[0] or pattern[0] == '.')
@@ -13,4 +14,3 @@ def is_match(text: str, pattern: str) -> bool:
 print(is_match("aa", "a"))  # False
 print(is_match("aa", "a*"))  # True
 print(is_match("ab", ".*"))  # True
-print(is_match("mississippi", "mis*is*p*."))  # False
